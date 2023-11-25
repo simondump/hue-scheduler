229f2ca8e5f6002c0ce5a93d5651e2a3ee446185
simondump <42521610+simondump@users.noreply.github.com>
2023-11-25 16:15:00 +0100

    Update build.sh

--- builder/build.sh
+++ builder/build.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
+set -e
 
 targets=("x86_64-unknown-linux-musl" "aarch64-unknown-linux-musl")
 
-for target in "${targets[@]}"; do
-  tag="ghcr.io/simondump/hue-scheduler:$target"
-  base="ghcr.io/rust-cross/rust-musl-cross:$target"
+echo "Tags: $DOCKER_METADATA_OUTPUT_TAGS"
+echo "Labels: $DOCKER_METADATA_OUTPUT_LABELS"
 
+for target in "${targets[@]}"; do
   docker buildx build \
     --file builder/Dockerfile \
-    --platform linux/amd64 \
-    --tag "$tag" \
-    --build-arg BASE="$base" \
-    --build-arg TARGET="$target" \. --push
+    --platform linux/amd64,linux/arm64 \
+    --tag "$DOCKER_METADATA_OUTPUT_TAGS" \
+    --label "$DOCKER_METADATA_OUTPUT_LABELS" \
+    --build-arg BASE="ghcr.io/rust-cross/rust-musl-cross:$target" \
+    --build-arg TARGET="$target" \
+    . --push
 
-  docker rmi "$base"
-  docker rmi "$tag"
+  docker rmi "$DOCKER_METADATA_OUTPUT_TAGS"
 done
