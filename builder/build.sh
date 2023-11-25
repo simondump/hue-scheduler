6d8f89cf81beb6cb12426801e20c612c7c1891ee
simondump <42521610+simondump@users.noreply.github.com>
2023-11-25 16:45:23 +0100

    Update build.sh

--- builder/build.sh
+++ builder/build.sh
@@ -9,9 +9,8 @@ for target in "${targets[@]}"; do
     --platform linux/amd64,linux/arm64 \
     --tag "$IMAGE_PATH:latest" \
     --tag "$IMAGE_PATH:$IMAGE_VERSION" \
+    --tag "$IMAGE_PATH:$IMAGE_VERSION_SHORT" \
     --build-arg BASE="ghcr.io/rust-cross/rust-musl-cross:$target" \
     --build-arg TARGET="$target" \
     . --push
-
-  docker rmi "$DOCKER_METADATA_OUTPUT_TAGS"
 done
