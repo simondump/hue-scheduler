43a0fcfc45e2895957e440e5303444824f8abf12
simondump <42521610+simondump@users.noreply.github.com>
2023-11-25 16:36:35 +0100

    Update build.sh

--- builder/build.sh
+++ builder/build.sh
@@ -3,16 +3,12 @@ set -e
 
 targets=("x86_64-unknown-linux-musl" "aarch64-unknown-linux-musl")
 
-echo "Tags: $DOCKER_METADATA_OUTPUT_TAGS"
-echo "Labels: $DOCKER_METADATA_OUTPUT_LABELS"
-
-# shellcheck disable=SC2046
 for target in "${targets[@]}"; do
   docker buildx build \
     --file builder/Dockerfile \
     --platform linux/amd64,linux/arm64 \
-    $(echo "$DOCKER_METADATA_OUTPUT_TAGS" | tr '\n' '--tag') \
-    $(echo "$DOCKER_METADATA_OUTPUT_LABELS" | tr '\n' '--label') \
+    --tag "$IMAGE_PATH:latest" \
+    --tag "$IMAGE_PATH:$IMAGE_VERSION" \
     --build-arg BASE="ghcr.io/rust-cross/rust-musl-cross:$target" \
     --build-arg TARGET="$target" \
     . --push
