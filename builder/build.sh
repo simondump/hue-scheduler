5177d48edf84c53bc22b52adaed29acd104763d3
simondump <42521610+simondump@users.noreply.github.com>
2023-11-25 15:59:39 +0100

    Update build.sh

--- builder/build.sh
+++ builder/build.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-declare -A map
+targets=("x86_64-unknown-linux-musl" "aarch64-unknown-linux-musl")
 
-map["x86_64-unknown-linux-musl"]="ghcr.io/gngpp/rust-musl-cross:x86_64-musl"
-map["aarch64-unknown-linux-musl"]="ghcr.io/gngpp/rust-musl-cross:aarch64-musl"
+for target in "${targets[@]}"; do
+  tag="ghcr.io/simondump/hue-scheduler:$target"
+  base="ghcr.io/rust-cross/rust-musl-cross:$target"
 
-for key in "${!map[@]}"; do
   docker buildx build --platform linux/amd64 \
-    --tag ghcr.io/simondump/hue-scheduler:"$key" \
-    --build-arg BASEIMAGE="${map[$key]}" \
-    --build-arg TARGETCARGO="$key" \. --push
+    --tag "$tag" \
+    --build-arg BASE="$base" \
+    --build-arg TARGET="$target" \. --push
 
-  docker rmi ghcr.io/gngpp/ninja-builder:"$key"
-  docker rmi "${map[$key]}"
+  docker rmi "$base"
+  docker rmi "$tag"
 done
